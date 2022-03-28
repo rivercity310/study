a85c1e8b1a490926c714e5eafaab1726ed6dc2f6
seungsu <rivercity310.github.io>
2022-03-29 07:29:50 +0900

    2022/03/29

--- chap2_Ex2.py
+++ chap2_Ex2.py
@@ -20,26 +20,26 @@ def pprint(msg, A):
         print(line)
     print("")
 
-# 가우수 - 조단 소거법을 수행하는 함수
+# 가우스 - 조단 소거법을 수행하는 함수
 
 
 def gauss(A):
     (n, m) = A.shape
 
     for i in range(0, min(n, m)):
-        # 1번째 열에서 절댓값이 최대인 성분의 행을 선택한다
+        # 1번째 열에서 절댓값이 최대인 성분의 행 선택
         maxEl = abs(A[i, i])
-        maxRow = 
+        maxRow = i
         for k in range(i + 1, n):
-            if abs(A[k, i]) > maxEl:
+            if (abs(A[k, i]) > maxEl):
                 maxEl = abs(A[k, i])
                 maxRow = k
 
-        # 현재 1번째 행과 최댓값을 갖는 행 maxRow 교환
+        # 현재 1번째 행과 최댓값을 갖는 행 maxRow를 교환
         for k in range(i, m):
-            tmp = A[maxRow, k]
+            temp = A[maxRow, k]
             A[maxRow, k] = A[i, k]
-            A[i, k] = tmp
+            A[i, k] = temp
 
         # 추축성분(피벗)을 1로 만들기
         piv = A[i, i]
@@ -51,17 +51,18 @@ def gauss(A):
             if k != i:
                 c = A[k, i] / A[i, i]
                 for j in range(i, m):
-                    if (i == j):
+                    if i == j:
                         A[k, j] = 0
                     else:
                         A[k, j] = A[k, j] - c * A[i, j]
 
+        # 중간과정 출력
         pprint(str(i + 1) + "번째 반복", A)
 
     # Ax = b의 해 반환
     x = np.zeros(m - 1)
-    for i in range(0, m-1):
-        x[i] = A[i, m-1]
+    for i in range(0, m - 1):
+        x[i] = A[i, m - 1]
 
     return x
 
