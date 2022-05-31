0f42a9297fea20d3257a24acc4f9bf072823ff74
seungsu <rivercity310.github.io>
2022-06-01 05:31:47 +0900

    2022/06/01

---
+++ chap5_Ex1.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+'''
+임의의 크기의 정방행렬에 대한 행렬식을 계산하는 프로그램 작성
+'''
+
+
+# 행렬 A의 i행과 j열을 제거한 행렬 생성
+def getMinorMatrix(A, i, j):
+    n = len(A)
+    M = np.zeros((n - 1, n - 1))
+
+    for a in range(0, n - 1):
+        k = a if (a < i) else a + 1
+        for b in range(0, n - 1):
+            l = b if (b < j) else b + 1
+            M[a, b] = A[k, l]
+
+    return M
+
+
+# 행렬식 계산
+def determinant(M):
+    if (len(M) == 2):           # 2 by 2 행렬이라면
+        return M[0, 0] * M[1, 1] - M[0, 1] * M[1, 0]
+
+    # 여인수를 이용하여 행렬식 계산하기 [ C_ij = {-1 ^ (i + j)} * A_ij ]
+    # determinant = (a_11 * c_11) + (a_12 * c_12) .....
+    detVal = 0
+    for c in range(len(M)):
+        detVal += ((-1)**c) * M[0, c] * determinant(getMinorMatrix(M, 0, c))
+
+    return detVal
+
+
+if __name__ == "__main__":
+    A = np.array([[-4, 0, 2, -1, 0], [1, 3, -3, -1, 4], [2, 0, 1, 3, 0],
+                  [-2, 1, -3, -1, 5], [1, -5, 1, 0, 5]])
+    print("<행렬 A>\n", A)
+    print("det(A) = ", determinant(A))
+
+    print()
+
+    B = np.array([[1, 3], [2, 4]])
+    print("<행렬 B>\n", B)
+    print("det(B) = ", determinant(B))
+
+    print()
+
+    C = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print("<행렬 C>\n", C)
+    print("det(C) = ", determinant(C))
--- diet.py
+++ diet.py
@@ -14,16 +14,16 @@ def print_nutritions():
     print("{0}님의 BMI 수치는 {1:.2f}입니다.".format(name, BMI))
 
     if BMI > 25:
-        print("BMI {0:.2f}은(는) 비만에 해당합니다.".format(BMI))
+        print("BMI {0:.2f}은(는) <비만>에 해당합니다.".format(BMI))
         print("식습관을 개선하고 유산소 운동과 근력운동을 겸해야합니다.")
     elif BMI > 23:
-        print("BMI {0:.2f}은(는) 과체중에 해당합니다.".format(BMI))
+        print("BMI {0:.2f}은(는) <과체중>에 해당합니다.".format(BMI))
         print("조금만 더 체중을 감량해보세요!")
     elif BMI > 18.5:
-        print("BMI {0:.2f}은(는) 정상에 해당합니다.".format(BMI))
+        print("BMI {0:.2f}은(는) <정상>에 해당합니다.".format(BMI))
         print("잘하고있어요!")
     else:
-        print("BMI {0:.2f}은(는) 저체중에 해당합니다.".format(BMI))
+        print("BMI {0:.2f}은(는) <저체중>에 해당합니다.".format(BMI))
         print("체중을 조금 늘려보세요!")
 
     print("------------------------------------------------------------------")
@@ -34,10 +34,10 @@ def print_nutritions():
     weight_interval = weight - goal_weight
 
     if weight_interval >= 0:
-        print("BMI {0:.2f}이(가) 되려면 {1:.2f}kg을 감량해야해요!".format(
+        print("BMI {0:.2f}이(가) 되려면 <{1:.2f}kg 감량> 해야해요!".format(
             goal_BMI, weight_interval))
     else:
-        print("BMI {0:.2f}이(가) 되려면 {1:.2f}kg을 증량해야해요!".format(
+        print("BMI {0:.2f}이(가) 되려면 <{1:.2f}kg 증량> 해야해요!".format(
             goal_BMI, -weight_interval))
     print("현재 체중: {0:.2f}kg\t목표 체중: {1:.2f}kg".format(weight, goal_weight))
     print()
