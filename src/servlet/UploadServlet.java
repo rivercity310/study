67b30168d0b42680591e2fa8098bcfe4e8fa95a4
seungsu <h970126@gmail.com>
2022-09-27 00:03:27 +0900

    2022/09/27

--- src/servlet/UploadServlet.java
+++ src/servlet/UploadServlet.java
@@ -8,38 +8,36 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
 
-@WebServlet("UploadServlet")
-@MultipartConfig(location = "C:\\Users\\h9701\\Desktop\\eclipse-ent\\work-space\\example\\web\\ajax\\uploaded",
-    maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 5 * 5
-)
+@WebServlet(name = "UploadServlet", value = "/UploadServlet")
+@MultipartConfig(location = "C:/Users/h9701/Desktop/eclipse-ent/uploadtest")
 public class UploadServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html; charset=utf-8");
         PrintWriter out = response.getWriter();
         request.setCharacterEncoding("utf-8");
 
-        String path = "C:\\Users\\h9701\\Desktop\\eclipse-ent\\work-space\\example\\web\\ajax\\uploaded";
+        String path = "C:/Users/h9701/Desktop/eclipse-ent/uploadtest";
         File isDir = new File(path);
-        if (!isDir.isDirectory()) isDir.mkdirs();
 
         Collection<Part> parts = request.getParts();
         for (Part part : parts) {
             if (part.getContentType() != null) {
                 String fileName = part.getSubmittedFileName();
-
                 if (fileName != null) {
-                    part.write(fileName.substring(0, fileName.lastIndexOf("."))
-                            + "_" + System.currentTimeMillis() + fileName.substring(fileName.lastIndexOf(".")));
-
-                    out.print("<br/>업로드한 파일 이름 : " + fileName);
-                    out.print("<br/>파일 크기 : " + part.getSize());
+                    part.write(fileName.substring(0, fileName.lastIndexOf(".")) + "_" + System.currentTimeMillis() + fileName.substring(fileName.lastIndexOf(".")));
+                    out.print("<br>업로드한 파일 이름 : " + fileName);
+                    out.print("<br>파일 크기 : " + part.getSize());
                 }
-                else {
-                    String partName = part.getName();
-                    String fieldValue = request.getParameter(partName);
-                    out.print("<br/>" + partName + " : " + fieldValue);
-                }
+            } else {
+                String partName = part.getName();
+                String fieldValue = request.getParameter(partName);
+                out.print("<br>" + partName + " : " + fieldValue);
             }
         }
 
