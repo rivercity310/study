2dbe569a26331530a61f09a32350ca715e7a790a
Seungsu <h970126@gmail.com>
2022-10-25 02:47:08 +0900

    Spring Setup File

--- Spring/hello-spring/src/main/java/polymorphism/SamsungTV.java
+++ Spring/hello-spring/src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,13 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+    public void initMethod() {
+        System.out.println("객체 초기화 작업 처리....");
+    }
+
+    public void destroyMethod() {
+        System.out.println("객체 삭제 전에 처리할 로직 처리...");
+    }
     public SamsungTV() {
         System.out.println("SamsungTV -- 객체 생성");
     }
--- Spring/hello-spring/src/main/java/polymorphism/TVUser.java
+++ Spring/hello-spring/src/main/java/polymorphism/TVUser.java
@@ -18,11 +18,12 @@ public class TVUser {
 
         /* Spring 컨테이너로부터 필요한 객체를 요청(Look up)한다. */
         AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
-        TV tv = (TV)factory.getBean("tv");
 
-        tv.powerOn();
-        tv.volumeUp();
-        tv.volumeDown();
-        tv.powerOff();
+        TV tv1 = (TV)factory.getBean("tv");
+        TV tv2 = (TV)factory.getBean("tv");
+        TV tv3 = (TV)factory.getBean("tv");
+
+        // 스프링 컨테이너 종료
+        factory.close();
     }
 }
