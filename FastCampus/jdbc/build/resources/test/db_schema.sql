88e618524f1d7d3cf1168fdbfb15f8a3e47ed3f0
seungsu <h970126@gmail.com>
2023-02-19 22:40:53 +0900

    del

--- FastCampus/jdbc/build/resources/test/db_schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS USERS;
-
-CREATE TABLE USERS (
-    userId      VARCHAR(12)     NOT NULL,
-    password    VARCHAR(12)     NOT NULL,
-    name        VARCHAR(28)     NOT NULL,
-    email       VARCHAR(50),
-    PRIMARY KEY(userId)
-)
