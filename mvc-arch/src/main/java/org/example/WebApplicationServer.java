30ae191b621c4cfb24ffebcfad2558f5968e232a
seungsu <h970126@gmail.com>
2023-02-23 02:50:02 +0900

    Feature: DispatcherServlet, HandlerMapping, Controller 단순 구현

--- mvc-arch/src/main/java/org/example/WebApplicationServer.java
+++ mvc-arch/src/main/java/org/example/WebApplicationServer.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
 public class WebApplicationServer {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+
+    public static void main(String[] args) throws LifecycleException {
+        String webappDirLocation = "webapps/";
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+
+        log.info("Configuring app with basedir : {}", new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
---
+++ mvc-arch/webapps/home.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+</head>
+<body>
+Home Page.....
+</body>
+</html>
