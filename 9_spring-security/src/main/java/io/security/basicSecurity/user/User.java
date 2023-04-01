c62c8cfa0d6c80dd4492734e33829bb54de0469a
seungsu <h970126@gmail.com>
2023-04-01 20:28:36 +0900

    사용자 관리 흐름

---
+++ 9_spring-security/src/main/java/io/security/basicSecurity/user/User.java
@@ -0,0 +1,27 @@
+package io.security.basicSecurity.user;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "security_user")
+@Entity
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String authority;
+
+    @Builder
+    public User(String username, String password, String authority) {
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
+    }
+}
